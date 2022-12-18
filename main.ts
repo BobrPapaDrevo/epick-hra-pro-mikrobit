373e837ad32ea5d3d9824908a06343fe5a7b1165
BobrPapaDrevo <115144163+BobrPapaDrevo@users.noreply.github.com>
2022-12-18 19:50:00 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,56 @@
+function getInput(){
+    return input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)
+}
+
+let roundInProgress = false
+let randTime = 0
+
+function round(){
+
+    randTime = randint(500, 5000)
+    roundInProgress = true
+    music.playTone(Note.C, randTime)
+
+}
+
+round()
+
+
+let wp = getInput()
+let startTime
+let endTime
+
+while (true){
+    if (getInput()){
+        if (!wp && roundInProgress){
+            startTime = control.millis()
+        }
+        wp = true
+    }else{
+        if (wp && roundInProgress){
+            
+            endTime = control.millis()
+            roundInProgress = false
+            let score = ""
+
+            if (Math.abs((endTime - startTime)-randTime) >= 1000){
+                score = "NIC MOC"
+            } else if (Math.abs((endTime - startTime) - randTime) >= 750){
+                score = "DECENTNI"
+            } else if (Math.abs((endTime - startTime) - randTime) >= 400) {
+                score = "DOBRE"
+            } else if (Math.abs((endTime - startTime) - randTime) >= 150) {
+                score = "UZASNE"
+            } else if (Math.abs((endTime - startTime) - randTime) <= 75) {
+                score = "PERFEKTNI"
+            }
+
+            
+            basic.showString(score)
+            basic.pause(500)
+
+            round()
+        }
+        wp = false
+    }
+}
